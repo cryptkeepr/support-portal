319392a100b49f3a633c44fef7499141774508eb
Eric Fledderman <ericfledderman@me.com>
2019-08-06 17:22:32 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang=en_US">
+<head>
+  <meta charset="utf-8" />
+  <title>CryptKeepr | Portal | Support</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" />
+</head>
+<body>
+  <!--[if IE]>
+    <p>
+      You are using an <strong>outdated</strong> browser.
+      Please <a href="https://browsehappy.com/">upgrade your browser</a> to
+      improve your experience and security.
+    </p>
+  <![endif]-->
+  <div class="page-container">
+    <h1>Support</h1>
+  </div>
+</body>
+</html>
