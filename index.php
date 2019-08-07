f33059d6e86e69cc464c1c41e946dfafa5a05297
Eric Fledderman <ericfledderman@me.com>
2019-08-07 00:12:26 +0000

    Work in progress

--- index.php
+++ index.php
@@ -17,6 +17,30 @@
   <![endif]-->
   <div class="page-container">
     <h1>Support</h1>
+    <h3>Frequently Asked Questions</h3>
+    <p>The quickest way to get the answer's to those questions that users most frequently aski</p>
+    <h5>General</h5>
+    <ul>
+      <li>
+        <h6>Lorem</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor et tortor at elementum. Quisque malesuada rhoncus ipsum, euismod laoreet dolor dictum et. Morbi non urna et arcu tincidunt auctor sit amet non nunc. Vivamus ac blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent quis vehicula est. Nullam velit dui, elementum finibus pharetra sed, efficitur sed elit. Duis mi neque, tristique et egestas nec, faucibus eu lectus. Integer elit orci, auctor sed tortor vel, sollicitudin consectetur ipsum. Vestibulum vulputate, velit at egestas ultrices, ante est facilisis ligula, vel semper nibh leo vel neque. In hac habitasse platea dictumst. Nunc semper in lectus eu interdum. Sed nec facilisis dui, eu aliquam tellus.</p>
+      </li>
+      <li>
+        <h6>Ipsum</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor et tortor at elementum. Quisque malesuada rhoncus ipsum, euismod laoreet dolor dictum et. Morbi non urna et arcu tincidunt auctor sit amet non nunc. Vivamus ac blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent quis vehicula est. Nullam velit dui, elementum finibus pharetra sed, efficitur sed elit. Duis mi neque, tristique et egestas nec, faucibus eu lectus. Integer elit orci, auctor sed tortor vel, sollicitudin consectetur ipsum. Vestibulum vulputate, velit at egestas ultrices, ante est facilisis ligula, vel semper nibh leo vel neque. In hac habitasse platea dictumst. Nunc semper in lectus eu interdum. Sed nec facilisis dui, eu aliquam tellus.</p>
+      </li>
+    </ul>
+    <h5>The Apps</h5>
+    <ul>
+      <li>
+        <h6>Plex</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor et tortor at elementum. Quisque malesuada rhoncus ipsum, euismod laoreet dolor dictum et. Morbi non urna et arcu tincidunt auctor sit amet non nunc. Vivamus ac blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent quis vehicula est. Nullam velit dui, elementum finibus pharetra sed, efficitur sed elit. Duis mi neque, tristique et egestas nec, faucibus eu lectus. Integer elit orci, auctor sed tortor vel, sollicitudin consectetur ipsum. Vestibulum vulputate, velit at egestas ultrices, ante est facilisis ligula, vel semper nibh leo vel neque. In hac habitasse platea dictumst. Nunc semper in lectus eu interdum. Sed nec facilisis dui, eu aliquam tellus.</p>
+      </li>
+      <li>
+        <h6>Ombi</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porttitor et tortor at elementum. Quisque malesuada rhoncus ipsum, euismod laoreet dolor dictum et. Morbi non urna et arcu tincidunt auctor sit amet non nunc. Vivamus ac blandit elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent quis vehicula est. Nullam velit dui, elementum finibus pharetra sed, efficitur sed elit. Duis mi neque, tristique et egestas nec, faucibus eu lectus. Integer elit orci, auctor sed tortor vel, sollicitudin consectetur ipsum. Vestibulum vulputate, velit at egestas ultrices, ante est facilisis ligula, vel semper nibh leo vel neque. In hac habitasse platea dictumst. Nunc semper in lectus eu interdum. Sed nec facilisis dui, eu aliquam tellus.</p>
+      </li>
+    </ul>
   </div>
 </body>
 </html>
